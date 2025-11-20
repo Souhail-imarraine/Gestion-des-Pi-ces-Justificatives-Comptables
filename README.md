ad7f3ca415996a83f1791531932675396c3b7e2f
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2025-11-20 14:06:38 +0100

    Setup initial project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# Gestion-des-Pi-ces-Justificatives-Comptables
---
+++ src/test/java/com/alamanedocs/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamanedocs.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
